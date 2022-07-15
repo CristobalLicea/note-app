74c2328d437bd02df2898668d8e38b8933227a01
Cristobal Licea <cris.licea@gmail.com>
2022-07-14 19:16:36 -0500

    add Post

--- server.js
+++ server.js
@@ -15,5 +15,15 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
+app.post('/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync('../db/db.json'))
+  const newNote = req.body;
+  newNote.id = uuid.v4();
+  notes.push(newNote)
+  fs.writeFileSync("../db/db.json", JSON.stringify(notes))
+}
+);
+
+
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
